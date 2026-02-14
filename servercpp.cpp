901a79c547acdaa4ea46cbf4da8079dda66f970a
ulisses888 <ulissesjr2002@gmail.com>
2026-02-14 19:36:21 -0300

    Prototipo funcao IN

--- servercpp.cpp
+++ servercpp.cpp
@@ -2,14 +2,17 @@
 #include <string>
 #include <list>
 #include <mutex>
+#include <shared_mutex>
 #include <thread>
+#include <condition_variable>
 
 struct Tupla {
     std::string chave;
     std::string valor;
 };
 
-std::mutex bloqueador;
+std::shared_mutex bloqueador;
+std::condition_variable cv;
 std::list<Tupla> listaTuplas;
 
 void WR(std::string chave, std::string valor);
@@ -22,18 +25,36 @@ int main(){
 }
 
 void WR(std::string chave, std::string valor){
-
-    listaTuplas.emplace_front(Tupla{chave,valor});
-
+    std::unique_lock lock(bloqueador);
+    listaTuplas.emplace_back(Tupla{chave,valor});
+    std::unique_lock unlock(bloqueador);
+    //lembrar d notificar
     return;
 }
 
 std::string RD(std::string chaveBusca){
-    
-    for (auto it = listaTuplas.rbegin(); it != listaTuplas.rend(); ++it) {
+    std::shared_lock lock(bloqueador);
+    for (auto it = listaTuplas.begin(); it != listaTuplas.end(); ++it) {
         if (it->chave == chaveBusca) {
             return it->valor;
         }
     }
+    lock.unlock();
+    //cv.wait();
     return "erro";
-} 
+}
+
+std::string IN(std::string chaveBusca){
+    std::unique_lock lock(bloqueador);
+    std::string valor =  NULL;
+    for (auto it = listaTuplas.begin(); it != listaTuplas.end(); ++it) {
+        if (it->chave == chaveBusca) {
+            valor = it->valor;
+            listaTuplas.erase(it);
+            return valor;
+        }
+    }
+    lock.unlock();
+    //aq vai ficar esperando tbm
+    return "blabla";
+}
