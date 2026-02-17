e99046ed5ff2780764249924b249ebdc95b204c4
ulisses888 <ulissesjr2002@gmail.com>
2026-02-17 14:50:42 -0300

    EX

--- servercpp.cpp
+++ servercpp.cpp
@@ -5,6 +5,7 @@
 #include <shared_mutex>
 #include <thread>
 #include <condition_variable>
+#include <algorithm>
 
 struct Tupla {
     std::string chave;
@@ -20,11 +21,16 @@ std::string WR(std::string chave, std::string valor);
 std::string RD(std::string chave);
 std::string IN(std::string chave);
 std::string EX(std::string chaveEntrada,std::string chaveSaida,std::string servico);
+std::string servico1(std::string valor);
+std::string servico2(std::string valor);
+std::string servico3(std::string valor);
 
 int main(){
     //std::thread t1(RD,"123");
     //std::thread t2(WR,"123","87654321");
-
+    //std::string teste = "testando maisculo";
+    //std::cout << "Antes:  " << teste << std::endl;
+    //std::cout << "Depois: " << servico3(teste) << std::endl; 
     //t1.join();
     //t2.join();
 
@@ -70,12 +76,49 @@ std::string IN(std::string chaveBusca){
 }
 
 std::string EX(std::string chaveEntrada,std::string chaveSaida,std::string servico){
-    std::unique_lock lock(bloqueador);
-    
-    for( auto it = listaTuplas.begin(); it != listaTuplas.end(); ++it){
-        if(it->chave == chaveEntrada){
-            std::string valor = it->valor;
+    unsigned short int servicos = std::stoi(servico);
+    for(int i = 0; i < 3; i++){
+        if(listaservicos[i] == servicos){
+            goto temServico;
         }
     }
+    return "NO-SERVICE";
+    temServico:
+    std::unique_lock lock(bloqueador);
+    while(true){
+        for(auto it = listaTuplas.begin(); it != listaTuplas.end(); ++it){
+            if(it->chave == chaveEntrada){
+                it->chave = chaveSaida;
+                switch (servicos)
+                {
+                    case 1:
+                        it->valor = servico1(it->valor);
+                        break;
+                    case 2:
+                        it->valor = servico2(it->valor);
+                        break;
+                    case 3:
+                        it->valor = servico3(it->valor);
+                        break;
+                
+                }
+                return "OK";
+            }
+        }
+        cv.wait(bloqueador);
+    }
+}
 
+std::string servico1(std::string valor){
+    std::transform(valor.begin(),valor.end(),valor.begin(),[](unsigned char c){ return std::toupper(c); });
+    return valor;
+}
+
+std::string servico2(std::string valor){
+    std::reverse(valor.begin(),valor.end());
+    return valor;
+}
+
+std::string servico3(std::string valor){
+    return std::to_string(valor.size());
 }
