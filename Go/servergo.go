4afa4f0dcbb81e0a5b5b3c4f143a0d6f2cf59cb7
ulisses888 <ulissesjr2002@gmail.com>
2026-02-18 16:56:19 -0300

    Testando

--- Go/servergo.go
+++ Go/servergo.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -40,6 +42,12 @@ func main() {
 		ChaveSaida: "",
 		Servico:    "",
 	}
+
+	mensagens <- Mensagem{
+		Operacao: "RD",
+		Chave:    "teste",
+	}
+
 	close(mensagens)
 	wg.Wait()
 }
@@ -47,14 +55,56 @@ func main() {
 func espacoTuplas(mensagens chan Mensagem, wg *sync.WaitGroup) {
 	//	mensagensEx :=make(chan MensagemEX)
 	defer wg.Done()
+
+	var lista []Tupla
+
 	for msg := range mensagens {
 		switch msg.Operacao {
 		case "WR":
-			fmt.Println("WR")
+			t := Tupla{
+				Chave: msg.Chave,
+				Valor: msg.Valor,
+			}
+			lista = append(lista, t)
+			fmt.Println(msg.Chave, "Escrito com sucesso")
 		case "RD":
-			fmt.Println("RD")
+			for _, tupla := range lista {
+				if tupla.Chave == msg.Chave {
+					fmt.Println("Leitura com sucesso: ", tupla.Valor)
+				}
+			}
+		case "IN":
+			for i, tupla := range lista {
+				if tupla.Chave == msg.Chave {
+					fmt.Println("Leitura IN com sucesso:", tupla.Valor)
+					lista = append(lista[:i], lista[i+1:]...)
+					break
+				}
+			}
+		case "EX":
+			servID, err := strconv.Atoi(msg.Servico)
+			if err != nil || (servID != 1 && servID != 2 && servID != 3) {
+				fmt.Println("Operacao Invalida")
+				break
+			}
+			for _, tupla := range lista {
+				if tupla.Chave == msg.Chave {
+					switch servID {
+					case 1:
+						tupla.Valor = strings.ToUpper(tupla.Valor)
+						tupla.Chave = msg.ChaveSaida
+					case 2:
+						//linguagem ferrada que n tem uma funcao de reverse para string
+					case 3:
+						fmt.Println(len(tupla.Valor))
+
+					}
+				}
+			}
+
+		default:
+			fmt.Println("Operacao invalida")
 		}
 
 	}
-
 }
